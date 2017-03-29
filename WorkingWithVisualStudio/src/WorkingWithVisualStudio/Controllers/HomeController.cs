32f4362df9e2193a1a5c1ec2ad1ba70cab0bd872
pkanev <pvkanev@yahoo.com>
2017-03-29 18:49:38 +0300

    Added exercise files for chapter 6 - working with visual studio

---
+++ WorkingWithVisualStudio/src/WorkingWithVisualStudio/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WorkingWithVisualStudio.Models;
+
+namespace WorkingWithVisualStudio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() 
+            => View(SimpleRepository.SharedRepository.Products.Where(p => p?.Price < 50));
+    }
+}
