dc7613a2e13b5c0e22d44f9bda285db7fe8aaa63
pkanev <pvkanev@gmail.com>
2017-03-29 07:27:30 +0300

    Added Party Invites project from chapter 2

---
+++ PartyInvites/src/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good morning" : "Good afternoon";
+            return View("MyView");
+        }
+
+        [HttpGet]
+        public IActionResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            Repository.AddResponse(guestResponse);
+            return View("Thanks", guestResponse);
+        }
+
+        [HttpGet]
+        public IActionResult ListResponses()
+        {
+            var responses = Repository.Responses.Where(r => r.WillAttend == true);
+            return View(responses);
+        }
+    }
+}
