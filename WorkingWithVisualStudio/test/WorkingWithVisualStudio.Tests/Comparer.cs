c6f64834f0f71dbc2da12185fc60bed1f379013c
pkanev <pvkanev@gmail.com>
2017-03-30 08:23:01 +0300

    Started working on chapter 7 - unit tests

---
+++ WorkingWithVisualStudio/test/WorkingWithVisualStudio.Tests/Comparer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WorkingWithVisualStudio.Tests
+{
+    public class Comparer
+    {
+        public static Comparer<U> Get<U>(Func<U, U, bool> func)
+        {
+            return new Comparer<U>(func);
+        }
+    }
+
+    public class Comparer<T> : Comparer, IEqualityComparer<T>
+    {
+        private Func<T, T, bool> comparissonFunction;
+
+        public Comparer(Func<T, T, bool> func)
+        {
+            this.comparissonFunction = func;
+        }
+
+        public bool Equals(T x, T y)
+        {
+            return this.comparissonFunction(x, y);
+        }
+
+        public int GetHashCode(T obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
