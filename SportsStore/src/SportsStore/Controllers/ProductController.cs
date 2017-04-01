f8b38c990da032a89f1e4ffab0de11f556768e3b
pkanev <pvkanev@gmail.com>
2017-04-01 10:00:30 +0300

    Created the backbone MVC for the sports system

---
+++ SportsStore/src/SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+
+        public ProductController(IProductRepository repo)
+        {
+            this.repository = repo;
+        }
+
+        public ViewResult List() => View(this.repository.Products);
+    }
+}
