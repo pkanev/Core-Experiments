09f8411d690019b28db4c155699cbf798cf011f1
pkanev <pvkanev@gmail.com>
2017-04-09 18:14:20 +0300

    Completed exercises from chapter 15 - routing

--- UrlsAndRoutes/src/UrlsAndRoutes/Controllers/CustomerController.cs
+++ UrlsAndRoutes/src/UrlsAndRoutes/Controllers/CustomerController.cs
@@ -6,6 +6,7 @@ namespace UrlsAndRoutes.Controllers
 {
     public class CustomerController : Controller
     {
+        [Route("app/[controller]/actions/[action]/{id:weekday?}")]
         public ViewResult Index() => View("Result",
             new Result
             {
@@ -13,11 +14,18 @@ namespace UrlsAndRoutes.Controllers
                 Action = nameof(Index)
             });
 
-        public ViewResult List() => View("Result",
-            new Result
+        public ViewResult List(string id)
+        {
+            Result r = new Result
             {
                 Controller = nameof(CustomerController),
                 Action = nameof(List)
-            });
+            };
+
+            r.Data["id"] = id ?? "<no value>";
+            r.Data["catchall"] = RouteData.Values["catchall"];
+
+            return View("Result", r);
+        }
     }
 }
--- UrlsAndRoutes/src/UrlsAndRoutes/Startup.cs
+++ UrlsAndRoutes/src/UrlsAndRoutes/Startup.cs
@@ -1,33 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Routing.Constraints;
+
+using UrlsAndRoutes.Infrastructure;
 
 namespace UrlsAndRoutes
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<RouteOptions>(options => options.ConstraintMap.Add("weekday", typeof(WeekDayConstraint)));
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app)
         {
             app.UseStatusCodePages();
             app.UseDeveloperExceptionPage();
             app.UseStaticFiles();
             app.UseMvc(routes =>
             {
-                routes.MapRoute(name: "MyRoute", template: "{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute(name: "MyRoute", 
+                    template: "{controller=Home}/{action=Index}/{id:weekday?}");
+
+                //routes.MapRoute(name: "MyRoute", 
+                //    template: "{controller}/{action}/{id?}", 
+                //    defaults: new { controller = "Home", action = "Index" },
+                //    constraints: new { id = new WeekDayConstraint() });
+                //routes.MapRoute(name: "MyRoute",
+                //    template: "{controller}/{action}/{id?}",
+                //    defaults: new { controller = "Home", action = "Index" },
+                //    constraints: new
+                //    {
+                //        id = new CompositeRouteConstraint(new IRouteConstraint[] {
+                //            new AlphaRouteConstraint(),
+                //            new MinLengthRouteConstraint(6)
+                //        })
+                //    });
+                //routes.MapRoute(name: "MyRoute", template: "{controller=Home}/{action=Index}/{id:alpha:minlength(6)?}");
+                //routes.MapRoute(name: "MyRoute", template: "{controller=Home}/{action=Index}/{id:range(10,20)?}");
+                //routes.MapRoute(name: "MyRoute", 
+                //    template: "{controller:regex(^H.*)=Home}/{action:regex(^Index$|^About$)=Index}/{id?}");
+                //routes.MapRoute(name: "MyRoute", template: "{controller:regex(^H.*)=Home}/{action=Index}/{id?}");
+                //routes.MapRoute(name: "MyRoute",
+                //    template: "{controller}/{action}/{id?}",
+                //    defaults: new { controller = "Home", action = "Index" },
+                //    constraints: new { id = new IntRouteConstraint() }
+                //);
+                //routes.MapRoute(name: "MyRoute", template: "{controller=Home}/{action=Index}/{id:int?}");
+                //routes.MapRoute(name: "MyRoute", template: "{controller=Home}/{action=Index}/{id?}/{*catchall}");
             });
         }
     }
